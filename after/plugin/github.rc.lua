fbdae8f8d1988fcffee5a033100e682524af35a5
hsingyingli <xingyingli855@gmail.com>
2023-01-25 18:01:25 +0800

    change github theme

--- after/plugin/github.rc.lua
+++ after/plugin/github.rc.lua
@@ -1,1 +1,9 @@
-require('github-theme').setup()
+require("github-theme").setup({
+  theme_style = "dark_default",
+  comment_style = "NONE",
+  keyword_style = "NONE",
+  function_style = "NONE",
+  variable_style = "NONE"
+  -- other config
+})
+
