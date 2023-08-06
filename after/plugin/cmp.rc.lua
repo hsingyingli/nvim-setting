14961c4556cc43241dbd2cf6ae96beae17e87b16
hsingyingli <xingyingli855@gmail.com>
2023-08-06 18:21:42 +0800

    change key map

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -17,7 +17,7 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-j>'] = cmp.mapping(function(fallback)
       local col = vim.fn.col('.') - 1
 
       if cmp.visible() then
@@ -29,7 +29,7 @@ cmp.setup({
       end
     end, {'i', 's'}),
 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-k>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item(select_opts)
       else
